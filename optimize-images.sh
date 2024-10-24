c66cc2b320a8f9fdaeb3baa4ba1b9ba7d95d0033
Antonio Fasolato <antonio.fasolato@gmail.com>
2024-10-24 15:25:27 +0200

    Skipped  images to not be converted

--- optimize-images.sh
+++ optimize-images.sh
@@ -3,8 +3,10 @@
 for FILENAME in src/img/*; do
     case $FILENAME in
         *.webp) continue;;
+        *screenshot-*) continue;;
+        *icon512_*) continue ;;
     esac
     F=$(basename -- "$FILENAME")
-    magick convert "$FILENAME" "src/img/${F%.*}.webp"
+    magick "$FILENAME" "src/img/${F%.*}.webp"
     rm "$FILENAME"
 done
